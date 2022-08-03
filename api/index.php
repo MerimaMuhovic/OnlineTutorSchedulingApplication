601bda3812da20de16357867880283a44dd35512
Merima Muhovic <merima.muhovic@stu.ibu.edu.ba>
2022-08-03 22:09:39 +0200

    Adding Delete functionality , updating middleware and UI

--- api/index.php
+++ api/index.php
@@ -5,12 +5,14 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 
+
 require dirname(__FILE__).'/../vendor/autoload.php';
 require dirname(__FILE__).'/dao/StudentDao.class.php';
 require dirname(__FILE__).'/dao/CourseDao.class.php';
 require dirname(__FILE__).'/services/CourseService.class.php';
 require dirname(__FILE__).'/services/StudentService.class.php';
 require dirname(__FILE__).'/services/TutorService.class.php';
+use \Firebase\JWT\JWT;
 
 
 /* utility function for reading query parameters from URL */
@@ -20,6 +22,29 @@ Flight::map('query', function($name, $default_value = NULL){
     $query_param = $query_param ? $query_param : $default_value;
     return $query_param;
   });
+
+  // middleware method for login
+/*Flight::route('/*', function(){
+  //return TRUE;
+  //perform JWT decode
+  $path = Flight::request()->url;
+  if ($path == '/students/login' || $path == '/docs.json' || $path == '/students/register') return TRUE; // exclude login route from middleware
+
+  $headers = getallheaders();
+  if (@!$headers['Authorization']){
+    Flight::json(["message" => "Authorization is missing"], 403);
+    return FALSE;
+  }else{
+    try {
+      $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+      Flight::set('course', $decoded);
+      return TRUE;
+    } catch (\Exception $e) {
+      Flight::json(["message" => "Authorization token is not valid"], 403);
+      return FALSE;
+    }
+  }
+}); */ 
 
   Flight::route('GET /swagger', function(){
     $openapi = @\OpenApi\scan(dirname(__FILE__)."/routes");
@@ -37,8 +62,8 @@ Flight::register('studentService', 'StudentService');
 Flight::register('tutorService', 'TutorService');
 
 /* Include all routes */
-require_once dirname(__FILE__)."/routes/students.php";
 require_once dirname(__FILE__)."/routes/middleware.php";
+require_once dirname(__FILE__)."/routes/students.php";
 require_once dirname(__FILE__)."/routes/tutors.php";
 require_once dirname(__FILE__)."/routes/courses.php"; 
 
